e1dcc7eba89395a981e0d177a87ebea9e0f9cda4
Robert Rothenberg <rrwo@cpan.org>
2020-12-02 14:11:24 +0000

    Commit Changes and bump $VERSION

--- lib/SemanticWeb/Schema.pm
+++ lib/SemanticWeb/Schema.pm
@@ -15,7 +15,7 @@ use Types::Standard qw/ Str /;
 
 use namespace::autoclean;
 
-our $VERSION = 'v11.0.0';
+our $VERSION = 'v11.0.1';
 
 # RECOMMEND PREREQ: aliased
 # RECOMMEND PREREQ: Class::XSAccessor 1.18
